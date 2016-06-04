12e933a837f3b5418cb3007b87e8a723b34a888e
Tannin <sherb@gmx.net>
2016-06-04 20:17:56 +0200

    translation

---
+++ src/game_fallout4_en.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>GameFallout4</name>
+    <message>
+        <location filename="gamefallout4.cpp" line="74"/>
+        <source>Adds support for the game Fallout 4.
+Splash by %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="fallout4gameplugins.cpp" line="62"/>
+        <source>Some of your plugins have invalid names! These plugins can not be loaded by the game. Please see mo_interface.log for a list of affected plugins and rename them.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
